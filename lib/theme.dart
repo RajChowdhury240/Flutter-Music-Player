83ad7b96bb7bf62b18da1102d16c3bde8d228d71
Raj Chowdhury <30806882+Rajchowdhury420@users.noreply.github.com>
2019-09-15 00:32:17 +0530

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -5,3 +5,4 @@ const Color lightAccentColor = const Color(0xFFFFAFAF);
 const Color darkAccentColor = const Color(0xFFD06F6F);
 const Color iconColor = Colors.grey;
 const Color xcolor = const Color(0xFFBFA197);
+
